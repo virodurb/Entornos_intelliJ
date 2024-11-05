daa46aaa78933d6255b9aab79f8449a3b64d7ecc
Virginia <vrodurb160@g.educaand.es>
2024-11-05 11:04:57 +0100

    inicio proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
     public class Main {
 
             public static void main(String[] args) {
-                System.out.println("Hola, mundo! Esta es una nueva funcionalidad.");
+                System.out.println("Hola, mundo! Esta es una nueva funcionalidad .");
             }
         }
 
